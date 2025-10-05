b7fcd0ba9f6ac3abaed003946cbad96e3d21ae74
Salman Fares <salmanfaresq@gmail.com>
2025-10-05 15:12:27 +0000

    Opened but not this interface pls

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type {NextConfig} from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   trailingSlash: true,
+  assetPrefix: isProd ? '/salmntic.github.io/' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
